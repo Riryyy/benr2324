dae2ca43ad283f2b97a5724da5c9ad7c3f9ddd6a
Adib <B022210262@student.utem.edu.my>
2024-03-28 15:24:42 +0800

    MongoDB basic functions

--- index.js
+++ index.js
@@ -13,3 +13,41 @@ app.listen(port, () => {
 })
 
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://Riry:adib@riry.tv5kopr.mongodb.net/?retryWrites=true&w=majority&appName=Riry";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    client.db("yeet").collection("f").insertOne({ 
+      name: "Riry",
+      email: "riry@riry",
+      age: 21 ,
+      status: "active",
+      faculty: "Engineering",
+
+    }  
+   );
+   console.log("Inserted a document into the collection");
+  } finally {
+
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
